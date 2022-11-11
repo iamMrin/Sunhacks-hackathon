29763d180b9488aafa90035f6ab48a7812543198
ArchisGokhale <archisgokhale001@gmail.com>
2022-11-12 04:32:34 +0530

    Complete WebScrape

--- webscrapper.py
+++ webscrapper.py
@@ -5,7 +5,9 @@ name=input("Enter Place: ")
 
 
 req = requests.get("https://en.wikipedia.org/wiki/"+name).text
+req1=requests.get("https://simple.wikipedia.org/wiki/"+name).text
 soup = bs(req, 'html.parser')
+soup1 = bs(req1, 'html.parser')
 latitude = soup.find("span", {"class": "latitude"})
 longitude = soup.find("span", {"class": "longitude"})
 
@@ -14,10 +16,9 @@ longitude = soup.find("span", {"class": "longitude"})
 lat = latitude.text #latitude
 long = longitude.text #longitude
 #first paragraph and second paragraph to be displayed 
-para=soup.find_all('p')
 
-
-
+#remove all numbers and special characters and brackets from the paragraph 0 and print
+para=soup1.find_all('p')
 newLat = lat.replace("°", ".").replace("′", "").replace("″", "").replace(" ", "").replace("N", "").replace("W", "").replace("E", "").replace("S", "")
 
 newLong = long.replace("°", ".").replace("′", "").replace("″", "").replace(" ", "").replace("N", "").replace("W", "").replace("E", "").replace("S", "")
@@ -37,21 +38,9 @@ dict = {
     "id":1,
     }
 }
-#OPEN INDEX.HTML WITH FILE OPERATIONS AND ADD TEXT TO IT ACCORDING TO THE FORMAT
-#ADD THE TEXT TO THE FILE
-#SAVE THE FILE
-f.open("index.html","w")
-f.write(dict)
-f.close()
-#OPEN THE FILE IN BROWSER
-import webbrowser
-webbrowser.open("index.html")
 
-# f=open("./index.html")
-# f.write(dict[id+1])
-# f.close()
-print(dict[name])
-print(dict)
+# print(dict[name])
+# print(dict)
 
 #print(n,o)
 
